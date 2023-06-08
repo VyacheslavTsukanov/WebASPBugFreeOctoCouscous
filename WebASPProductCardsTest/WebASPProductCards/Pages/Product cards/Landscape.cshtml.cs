9c91ec371c2fac6886bee4db36127cca9e1a5035
VyacheslavTsukanov <vtsukanoff@yandex.ru>
2023-06-08 10:50:52 +0400

    A project has been created and a Landscape page has been added

---
+++ WebASPProductCardsTest/WebASPProductCards/Pages/Product cards/Landscape.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebASPProductCards.Pages.Product_cards
+{
+    public class LandscapeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
